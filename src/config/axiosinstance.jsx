fada07b07ed8074b58e7fa705b1c9c35e6940f00
farhan sherin <farhana23sherin9023@gmail.com>
2025-11-23 19:50:50 +0530

    updated axios baseURL for production

--- src/config/axiosinstance.jsx
+++ src/config/axiosinstance.jsx
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true
 });
-
-
-
