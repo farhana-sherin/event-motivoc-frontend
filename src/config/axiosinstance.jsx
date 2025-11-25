e93cf80781ba29e78db841f39653fd9e3cb7fca2
farhan sherin <farhana23sherin9023@gmail.com>
2025-11-25 11:53:15 +0530

    updated the axios

--- src/config/axiosinstance.jsx
+++ src/config/axiosinstance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL+"/api/v1/",
+  baseURL: import.meta.env.VITE_API_BASE_URL, // no need to add /api/v1 again
   withCredentials: true
 });
